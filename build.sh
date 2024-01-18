d83ba44c41a7a51fd53778702866866d35dab899
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-01-18 11:19:19 -0800

    treewide: update project to BlocksDS v0.12.0
    - Updated Makefiles
    - Updated bootloader submodule for v0.12.0
    - Updated custom linkerscripts
    - Added project-specific `.specs` files for targets that use a custom
      linkerscript
    - Use `cc`/`c++` instead of invoking clang/clang++ directly to avoid
      cross-platform compilation issues

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-clang -o resource/dsbize/dsbize resource/dsbize/dsbize.c
-clang++ -o resource/r4denc/r4denc resource/r4denc/r4denc.cpp
+cc -o resource/dsbize/dsbize resource/dsbize/dsbize.c
+c++ -o resource/r4denc/r4denc resource/r4denc/r4denc.cpp
 mkdir -p data
 make -C bootloader LOADBIN=$PWD/data/load.bin NO_SDMMC=1
 make dist
